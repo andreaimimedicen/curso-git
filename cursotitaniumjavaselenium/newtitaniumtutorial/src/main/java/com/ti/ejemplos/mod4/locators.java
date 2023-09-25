ecd859093a81c16378559b8c390d9cfb4e14c134
andrea <handebi09@gmail.com>
2023-09-24 19:39:32 -0600

    using id and xpath

---
+++ cursotitaniumjavaselenium/newtitaniumtutorial/src/main/java/com/ti/ejemplos/mod4/locators.java
@@ -0,0 +1,35 @@
+package com.ti.ejemplos.mod4;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class locators {
+    static WebDriver driver;
+    static String URL = "http://uitestingplayground.com/home";
+    static String sendText = "your peanut butter and your tears";
+    static String actualResult = "";
+    static String actualResult2 = "";
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+
+        driver.findElement(By.xpath("//*[@href = '/textinput']")).click();
+        driver.findElement(By.id("newButtonName")).sendKeys(sendText);
+        driver.findElement(By.id("updatingButton")).click();
+
+        actualResult = driver.findElement(By.id("updatingButton")).getText();
+        System.out.println(actualResult.contains(sendText)?"Button was clicking and text is the expected": "FAILED! text is "+actualResult);
+
+        driver.findElement(By.id("newButtonName")).clear();
+        driver.findElement(By.id("newButtonName")).sendKeys("a different thing to sendText");
+        driver.findElement(By.id("updatingButton")).click();
+        actualResult2 = driver.findElement(By.id("updatingButton")).getText();
+        System.out.println(actualResult2.contains("a different thing to sendText")?"Button was clicking and text is the expected": "FAILED! text is "+actualResult);
+
+
+        driver.quit();
+    }
+}
